f839d07495d7b8eb5a75d28a9dcf2b821fa831c3
ryanmurphy01 <ryan.murphy01@stclairconnect.ca>
2022-03-09 17:10:57 -0500

    #10 Create CSS and layout for login page - Created layout and added css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,27 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
 {{-- import the bootstrap, as mentioned in that comment --}}
 @extends('template')
 {{-- marker to fill the contect yield section with whatever is put in here --}}
 @section('content')
 
-{{-- temp styles --}}
-<div class="container" style="height: 80vh">
-    <div class="row" style="padding-top: 10vh">
-        <div class="col-sm-4 col-sm-offset-4">
-            {{-- TODO link to proper constroller once ready --}}
-            <form action="login" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="loginEmail">Email address</label>
-                    <input type="email" name="email" class="form-control" id="loginEmail">
-                </div>
-                <div class="form-group">
-                    <label for="loginPassword">Password</label>
-                    <input type="password" name="password" class="form-control" id="loginPassword">
-                </div>
-                <button type="submit" class="btn btn-primary">Sign In</button>
-            </form>
+<head>
+
+</head>
+
+<body>
+
+<section class="vh-100" style="background-color: #00693C;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-sm-12 col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card shadow-2-strong" style="border-radius: 1rem;">
+          <div class="card-body p-5">
+
+            <h3 class="mb-4 fs-1 text-center">Login</h3>
+
+            <form action="" method="post">
+
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Password</label>
+            </div>
+            
+        
+              <p class="small d-md-flex justify-content-md-end"><a href="#!" class="text-black mb-2">Forgot password?</a></p>
+           
+              
+            <div class="d-grid">
+                <button type="button" class="btn btn-lg btn-primary mb-2">Login</button>
+            </div>
+           
+              </form>
+        
+
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
+</section>
+</body>
 
 @endsection
+
+
+</html>
