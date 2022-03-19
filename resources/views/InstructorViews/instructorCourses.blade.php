148d251131d45e2dbb9ee20de5ba9512aab5e7cd
SaadMAmjad <55886212+SaadMAmjad@users.noreply.github.com>
2022-03-19 04:45:58 -0400

    #75 #76 Styled the instructors course request page and made the program dropdown functional

--- resources/views/InstructorViews/instructorCourses.blade.php
+++ resources/views/InstructorViews/instructorCourses.blade.php
@@ -8,7 +8,7 @@
             {{-- extra style, border to make the bar actually visible lol --}}
             <div class="d-flex flex-column align-items-center align-items-sm-start px-1 pt-2 min-vh-100" style="border-right: 1px solid black;">
                 <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto link-dark text-decoration-none justify-center">
-                    <span class="d-none d-sm-inline text-center" style="font-size: 18pt;">Admin Panel</span>
+                    <span class="d-none d-sm-inline text-center" style="font-size: 18pt;">Instructor Panel</span>
                 </a>
                 {{-- make both of these max width so the active box fills all the space, also add a touch of margin so it doesn't touch the edge --}}
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu" style="width: 100%; margin-right: 10px">                    <li class="nav-item" style="width: 100%">
@@ -70,38 +70,59 @@
                 </div>
             </div>
         </div>
-        <div class="col py-3">
-            <label for="selectPrograms">Program</label>
-            <select name="selectPrograms" id="selectPrograms">
-                {{-- use some kind of foreach to pull from the database once that's setup, but for now placeholder --}}
-                <option value="B990">Mobile Applications Development</option>
-            </select>
+        <div class="col-8">
+            <h1 class="pb-5 pt-5 display-3">Select Your Courses</h1>
 
-            <label for="selectCourses">Courses</label>
-            <select name="selectCourses" id="selectCourses">
-                {{-- again some kind of foreach, probably with a condition checking for the matching program above --}}
-                {{-- also an 'on hover' here or something that checks program for updated value before showing coruses --}}
-                <option value="MAD201">Java Programming I</option>
-            </select>
+            <div class="row">
+                <div class="col-sm">
+                    <label class="pb-2 pt-2 display-6" for="selectPrograms">Program</label>
+                    <select class="form-select form-select-lg" name="selectPrograms" id="selectPrograms">
+                        <option value='' disabled selected>Select Program</option>
+                        @foreach ($programs as $program)
+                        <option value="{{ $program->id }}">{{ $program->program_name }}</option>
+                        @endforeach
+                    </select>
 
-            {{-- might need to make this whole section into a form, or have some other way to capture the course inputs --}}
-            {{-- also buttons may need ids --}}
-            <button type="button" class="btn btn-primary text-center">Add to List</button>
+                    <label class="pb-2 pt-2 display-6" for="selectCourses">Courses</label>
+                    <select class="form-select form-select-lg" name="selectCourses" id="selectCourses">
+                        {{-- again some kind of foreach, probably with a condition checking for the matching program above --}}
+                        {{-- also an 'on hover' here or something that checks program for updated value before showing coruses --}}
+                        <option value='' disabled selected>Select Course</option>
+                        {{-- <option value="MAD201">Java Programming I</option> --}}
+                    </select>
 
-            <p>
-                This course introduces students to the fundamental programming concepts using the Java language.
-                Students will learn the proper use of control structures such as selection looping and modularization.
-                They will also become proficient at using the basic Java syntax to implement program designs.
-                Students will learn how a language such as Java handles various primitive data types and how standard classes
-                are used to support the storage and manipulation of more complex data. Students will be introduced to "Encapsulation"
-                the first concept of Object Oriented Programming (OOP).
-            </p>
+                    {{-- might need to make this whole section into a form, or have some other way to capture the course inputs --}}
+                    {{-- also buttons may need ids --}}
+                    <button style="margin-top: 10px" type="button" class="pb-2 pt-2 btn btn-success">Add to Selection</button>
+                </div>
 
-            {{-- table to hold the selected courses --}}
-            <table>
-                {{-- replace the button text with icon, or just replace it entirely with an image and add an onclick --}}
-                <tr>Java Programming I - MAD201<td><button class="editbtn">edit</button></td></tr>
-            </table>
+                <div class="col-sm">
+                    <h3 class="pb-2 pt-2 display-6">Course Description</h3>
+                    <p>
+                        This course introduces students to the fundamental programming concepts using the Java language.
+                        Students will learn the proper use of control structures such as selection looping and modularization.
+                        They will also become proficient at using the basic Java syntax to implement program designs.
+                        Students will learn how a language such as Java handles various primitive data types and how standard classes
+                        are used to support the storage and manipulation of more complex data. Students will be introduced to "Encapsulation"
+                        the first concept of Object Oriented Programming (OOP).
+                    </p>
+                </div>
+                <div class="col-sm">
+                    <h3 class="pb-2 pt-2 display-6">Selected Courses</h3>
+                    {{-- table to hold the selected courses --}}
+                    <table class="table table-hover table-striped">
+                        {{-- replace the button text with icon, or just replace it entirely with an image and add an onclick --}}
+                        <td>Java Programming I - MAD201
+                            <button type="button" class="btn btn-danger float-end">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
+                                    <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
+                                </svg>
+                            </button>
+                        </td>
+                    </table>
+                </div>
+            </div>
+            <button type="button" class="btn btn-primary float-end">Request These Courses</button>
         </div>
     </div>
 </div>
