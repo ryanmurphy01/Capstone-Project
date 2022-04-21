c1b8dcb1a13687f864604751e54286e847fa3e34
SaadMAmjad <55886212+SaadMAmjad@users.noreply.github.com>
2022-04-21 05:17:53 -0400

    Removed the semester field from the requests table on admin's panel, since we are no longer making course selection specific to semesters

--- resources/views/AdminViews/adminDeniedRequests.blade.php
+++ resources/views/AdminViews/adminDeniedRequests.blade.php
@@ -165,7 +165,6 @@
                         <th>Instructor Name</th>
                         <th>Course Name</th>
                         <th>Course Code</th>
-                        <th>Semester</th>
                         <th>Status</th>
                         <th></th>
                     </tr>
@@ -176,8 +175,6 @@
                             <td>{{ $record->first_name }}  {{ $record->last_name }}</td>
                             <td>{{ $record->course_name }}</td>
                             <td>{{ $record->course_code }}</td>
-                            {{-- semester code --}}
-                            <td>{{ $record->code }}</td>
                             <td style="padding-left: 20px">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-x-square" viewBox="0 0 16 16">
                                     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
--- resources/views/AdminViews/adminRequests.blade.php
+++ resources/views/AdminViews/adminRequests.blade.php
@@ -165,7 +165,6 @@
                         <th>Instructor Name</th>
                         <th>Course Name</th>
                         <th>Course Code</th>
-                        <th>Semester</th>
                         <th>Status</th>
                         <th></th>
                     </tr>
@@ -176,8 +175,6 @@
                             <td>{{ $record->first_name }}  {{ $record->last_name }}</td>
                             <td>{{ $record->course_name }}</td>
                             <td>{{ $record->course_code }}</td>
-                            {{-- semester code --}}
-                            <td>{{ $record->code }}</td>
                             <td style="padding-left: 20px">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
